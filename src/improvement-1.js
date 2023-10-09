82bf47ece7fdd3916e1dc26eadec3683b36d3699
danielerat <davidodo2015@gmail.com>
2023-10-09 21:25:56 +0200

    bg:Fixing all the non passing tests

--- src/improvement-1.js
+++ src/improvement-1.js
@@ -5,12 +5,6 @@
  */
 
 // FUNCTION 1
-export default function cntRbt(ls) {
-  var cnt = 0;
-  for (var i = 0; i < ls.length; i++) {
-    if (ls[i] == "R") {
-      cnt++;
-    }
-  }
-  return cnt;
+export default function countNumberOfR(string) {
+  return string.match(/R/g).length;
 }
--- src/passwordValidation.js
+++ src/passwordValidation.js
@@ -6,21 +6,33 @@ export const forbiddenPasswords = ["amG84h6yeQ", "mc9Q20pdjH", "jnT6Q2f8U5"];
  * @param {string} password
  * @returns {boolean}
  */
+
+function hasConsecutive(str){
+  const consecutive="1234567890987654321";
+  const matches=str.match(/\d+/gi)?.filter(x=>x.length>3)
+  console.log(matches)
+ for(x of matches){
+      if(consecutive.startsWith(x)||consecutive.endsWith(x)) {return true }}
+ return false
+}
 export default function isValidPassword(password = "") {
   // The following line ensures, that password is always a string, like the number 128 -> string "128"
   if (typeof password !== "string") password = String(password);
+  //Length
+  if(password.length !== 10) return false;
+  
+if(!(/^[a-zA-Z\d]+$/.test(password))) return false
+  if(
+    !(/\d/.test(password)) ||
+    !(/[a-z]/.test(password)) ||
+    !(/[A-Z]/.test(password))) 
+   
+    return false
+    if (new Set([...password]).size<4) return false;
+  if(forbiddenPasswords.includes(password)) return false;
+  
+  if (new Set([...password]).size<4) return false;
 
-  // * * * YOUR CODE GOES IN HERE ... * * *
-  /*
-   * if (password is not exactly 10 digits or characters...) {
-   *   return ...;
-   * }
-   *
-   * if (is not composed by digits and numbers) {
-   *   return ...;
-   * }
-   */
-  const setOfPassword = new Set([...password]);
-  if (setOfPassword.size < 4) return false;
-  return true;
+    return true
+  
 }
